286b97ef66b4be1dacf1204fdd0ba5543f93f975
luoqiangwei <luoqiangwei@live.cn>
2018-09-17 20:59:16 +0800

    add puzzles.

--- Python/Td/a.py
+++ Python/Td/a.py
@@ -1,4 +1,14 @@
 num = int(input())
 #print(list(range(1, num + 1)))
-stack = list(range(1, num + 1))
+stack0 = list(range(1, num + 1))
 
+bug = 0
+stack = []
+
+while True:
+	result = ""
+	for i in range(0, len(stack)):
+		stack.push(stack0[i])
+		result += stack.pop(0) + " "
+	print(result)
+	
