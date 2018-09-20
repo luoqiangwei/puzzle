f2f1d96d3dc863916d181342002cbed467bf81d3
luoqiangwei <luoqiangwei@live.cn>
2018-09-20 23:52:29 +0800

    Fibonacci use buffer.

---
+++ C/FoundationsOfAlgorithms/A/Fibonacci/b.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int fib(int n);
+
+int main()
+{
+    printf("%d\n", fib(25));
+
+    return 0;
+}
+
+int fib(int n)
+{
+    int store[n+1];
+    store[0] = 0;
+    if (n > 0)
+    {
+        store[1] = 1;
+        for(int i = 2; i < n + 1; i++)
+        {
+            store[i] = store[i-1] + store[i-2];
+        }
+    }
+    return store[n];
+}
