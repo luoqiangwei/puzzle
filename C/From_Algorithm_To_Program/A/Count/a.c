cb2e536aa583a2a4ad4e4046122b7f3828aec554
luoqiangwei <luoqiangwei@live.cn>
2018-09-21 13:07:14 +0800

    completed.

--- C/From_Algorithm_To_Program/A/Count/a.c
+++ C/From_Algorithm_To_Program/A/Count/a.c
@@ -3,8 +3,8 @@
 #include <string.h>
 #include <assert.h>
 #include "strstream.h"
-
-
+#include "search.h"
+#include "compare.h"
 
 void search(FILE* in, FILE* out);
 int searchItem();
@@ -21,11 +21,73 @@ int main()
 
 void search(FILE* in, FILE* out)
 {
+    char first[80];
     assert(in && out);
     while(!feof(in))
     {
         int i;
         StrInputStream sin;
         char type, second[250];
+        initStrInputStream(&sin, first);
+        readChar(&sin, &type);
+        switch(type)
+        {
+            case 'i':
+                int key, a[80], n = 0;
+                readInt(&sin, &key);
+                fgets(second, 80, in);
+                initStrInputStream(&sin, second);
+                while(!sisEof(&sin))
+                {
+                    readInt(&sin, &a[n++]);
+                }
+                i = linearSearch(a, sizeof(int), n, &key, intGreater);
+                break;
+            case 'f':
+                int key, a[80], n = 0;
+                readDouble(&sin, &key);
+                fgets(second, 80, in);
+                initStrInputStream(&sin, second);
+                while(!sisEof(&sin))
+                {
+                    readDouble(&sin, &a[n++]);
+                }
+                i = linearSearch(a, sizeof(double), n, &key, doubleGreater);
+                break;
+            case 'c':
+                int key, a[80], n = 0;
+                readChar(&sin, &key);
+                fgets(second, 80, in);
+                initStrInputStream(&sin, second);
+                while(!sisEof(&sin))
+                {
+                    readChar(&sin, &a[n++]);
+                }
+                i = linearSearch(a, sizeof(char), n, &key, charGreater);
+                break;
+            case 's':
+                int key, a[80], n = 0;
+                readString(&sin, &key);
+                fgets(second, 80, in);
+                initStrInputStream(&sin, second);
+                while(!sisEof(&sin))
+                {
+                    readString(&sin, &a[n++]);
+                }
+                i = linearSearch(a, sizeof(char *), n, key, strcmp);
+                break;
+        }
+        if(i > -1)
+        {
+            fprintf(out, "%d\n", i);
+        }
+        else
+        {
+            fprintf(out, "not found!\r\n");
+        }
+        fgets(first, 80, in);
+        fclose(in);
+        fclose(out);
+        return 0;
     }
 }
