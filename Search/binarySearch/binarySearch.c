abe6a35cc3659d0bc4ba7959a925efeeaa7bd95a
luoqiangwei <luoqiangwei@live.cn>
2018-09-22 22:54:39 +0800

    add binarySearch C type.

--- Search/binarySearch/binarySearch.c
+++ Search/binarySearch/binarySearch.c
@@ -1,6 +1,24 @@
 #include "binarySearch.h"
 
-int binarySearch(int low, int high)
+key binarySearch(key *a, key x, key low, key high)
 {
-    
+    key mid = (low + high) / 2;
+    if (low > high)
+    {
+        return 0;
+    }
+    else{
+        if (a[mid] == x)
+        {
+            return mid;
+        }
+        else if (a[mid] > x)
+        {
+            return binarySearch(a, x, low, mid - 1);
+        }
+        else
+        {
+            return binarySearch(a, x, mid + 1, high);
+        }
+    }
 }
--- Search/binarySearch/binarySearch.h
+++ Search/binarySearch/binarySearch.h
@@ -1,3 +1,3 @@
 #pragma once
-
+typedef int key;
 int binarySearch(int low, int high);
