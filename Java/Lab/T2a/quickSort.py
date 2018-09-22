a4ab8ae528f7749d663562762c5a849ecfe87622
luoqiangwei <luoqiangwei@live.cn>
2018-09-22 18:40:25 +0800

    T2a completed.

--- Java/Lab/T2a/quickSort.py
+++ Java/Lab/T2a/quickSort.py
@@ -1,14 +1,14 @@
 def quickSort(L, low, high):
     l = low
     h = high
-    if(l > h):
+    if(l >= h):
         return L
     key = L[l]
     while l < h:
-        while L[h] >= key:
+        while l < h and L[h][1] >= key[1]:
             h -= 1
         L[l] = L[h]
-        while L[l] <= key:
+        while l < h and L[l][1] <= key[1]:
             l += 1
         L[h] = L[l]
     L[l] = key
