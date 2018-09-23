06ba90db8230832e7cc8befdf512f1cbb1fd7c9d
luoqiangwei <luoqiangwei@live.cn>
2018-09-23 14:11:45 +0800

    some change.

--- Python/Lab/T1d/a.py
+++ Python/Lab/T1d/a.py
@@ -3,12 +3,23 @@ num = int(input())
 stack0 = list(range(1, num + 1))
 
 bug = 0
+all = []
 stack = []
+n = 0
 
-while True:
-	result = ""
-	for i in range(0, len(stack)):
-		stack.push(stack0[i])
-		result += stack.pop(0) + " "
-	print(result)
-	
+def swap(a, b):
+	t = a
+	a = b
+	b = t
+
+def perm(list, k, m):
+	global n
+	if (k > m):
+		all.append(list)
+		n += 1
+	else:
+		for i in range(k, m + 1):
+			swap(list[k], list[i])
+			perm(list, k + 1, m)
+			swap(list[k], list[i])
+
