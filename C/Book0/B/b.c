99b8946fd772cae0a234c0b695af3a41293b32b7
luoqiangwei <luoqiangwei@live.cn>
2018-11-28 10:49:37 +0800

    BUG..

---
+++ C/Book0/B/b.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+char s[] = "123";
+
+int n = sizeof s / sizeof (char) - 1;
+
+void disppath(char path[], int d)
+{
+    printf(" 输出一个子集: {");
+    for(int i = 0; i < d; i++)
+        printf("%d, ", path[i]);
+    if(d >= 0) printf("%c", path[d]);
+    printf("}\n");
+}
+
+int dfs(char path[], int d, int start)
+{
+    if(start == n)
+        disppath(path, d);
+    else
+    {
+        dfs(path, d, start + 1);
+        d++;
+        path[d] = s[start];
+        dfs(path, d, start + 1);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int d = -1;
+    char path[100];
+    printf("s:%s\n", s);
+    dfs(path, d, 0);
+    return 0;
+}
